b8e34a6181d543f7c2da8a1330f667a03fd7c1c3
BimaAdi <bimaadi419@gmail.com>
2025-04-18 18:43:15 +0700

    add check using biome

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,11 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export function meta() {
+	return [{ title: "My Stack" }];
 }
 
 export default function Home() {
-  return <Welcome />;
+	return (
+		<main>
+			<h1 className="text-xl">Hello</h1>
+		</main>
+	);
 }
